bfbb90abeed7fa0e01018ca7a72ac57d183a8b45
farhanhere <113241435+farhanhere@users.noreply.github.com>
2023-06-02 11:46:18 +0500

    s

--- screens/Settings.js
+++ screens/Settings.js
@@ -1,70 +1,22 @@
-import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { auth } from './firebase';
+import React from 'react'
+import { StyleSheet, Text, TextInput, View, TouchableOpacity, Alert } from 'react-native';
 
-const Settings = () => {
-  const navigation = useNavigation();
-
-  const handleLogout = () => {
-    Alert.alert(
-      'Logout',
-      'Are you sure you want to logout?',
-      [
-        {
-          text: 'Cancel',
-          style: 'cancel',
-        },
-        {
-          text: 'Logout',
-          onPress: async () => {
-            try {
-              // Perform the logout operation
-              await auth.signOut();
-              navigation.navigate('Login');
-            } catch (error) {
-              console.log('Error logging out:', error.message);
-            }
-          },
-        },
-      ],
-      { cancelable: false }
+const Settings=()=> {
+    return (
+      <View style={styles.container}>
+        <Text style={{color:'white'}}>Settings!</Text>
+      </View>
     );
-  };
+  }
+  
+  const styles = StyleSheet.create({
+    container: {flex: 1, 
+      justifyContent: 'center',
+       alignItems: 'center',
+        backgroundColor: 'black'
+    },
+    
+  });
+  
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Settings!</Text>
-      <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
-        <Text style={styles.logoutText}>Logout</Text>
-      </TouchableOpacity>
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'black',
-  },
-  title: {
-    color: 'white',
-    fontSize: 24,
-    marginBottom: 20,
-  },
-  logoutButton: {
-    backgroundColor: '#E31C79',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 5,
-  },
-  logoutText: {
-    color: 'white',
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-});
-
-export { Settings };
+export  {Settings};
